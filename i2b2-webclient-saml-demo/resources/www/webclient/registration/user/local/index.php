46ccbf687f8b2601057d3f1ce60ae46e78baff9a
Kevin Bui <kvb2univpitt@gmail.com>
2021-08-26 12:17:45 -0400

    Added server-side data validation for user registration.

--- i2b2-webclient-saml-demo/resources/www/webclient/registration/user/local/index.php
+++ i2b2-webclient-saml-demo/resources/www/webclient/registration/user/local/index.php
@@ -6,8 +6,62 @@ require_once('../i2b2.php');
 
 date_default_timezone_set('America/New_York');
 
+function isValid() {
+    $hostname = trim(filter_input(INPUT_POST, 'hostName', FILTER_SANITIZE_STRING));
+    $firstName = trim(filter_input(INPUT_POST, 'firstName', FILTER_SANITIZE_STRING));
+    $lastName = trim(filter_input(INPUT_POST, 'lastName', FILTER_SANITIZE_STRING));
+    $email = trim(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING));
+    $username = trim(filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING));
+    $password = trim(filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING));
+    $confirmPassword = trim(filter_input(INPUT_POST, 'confirmPassword', FILTER_SANITIZE_STRING));
+    $agree = filter_input(INPUT_POST, 'agree', FILTER_SANITIZE_STRING);
+
+    $valid = true;
+
+    // make sure all inputs are there
+    if (strlen($hostname) == 0) {
+        $_SESSION['error_msg'] = "Hostname is required.";
+        $valid = false;
+    } else if (strlen($firstName) == 0) {
+        $_SESSION['error_msg'] = "First name is required.";
+        $valid = false;
+    } else if (strlen($lastName) == 0) {
+        $_SESSION['error_msg'] = "Last name is required.";
+        $valid = false;
+    } else if (strlen($email) == 0) {
+        $_SESSION['error_msg'] = "Email is required.";
+        $valid = false;
+    } else if (strlen($username) == 0) {
+        $_SESSION['error_msg'] = "Username is required.";
+        $valid = false;
+    } else if (strlen($password) == 0) {
+        $_SESSION['error_msg'] = "Password is required.";
+        $valid = false;
+    } else if (strlen($confirmPassword) == 0) {
+        $_SESSION['error_msg'] = "Confirm Password is required.";
+        $valid = false;
+    } else if (!$agree) {
+        $_SESSION['error_msg'] = "You must accept the terms and conditions.";
+        $valid = false;
+    }
+
+    // make sure email is valid
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $_SESSION['error_msg'] = "Invalid email format.";
+        $valid = false;
+    }
+
+    // make sure password and confirm password are equal
+    if (strcmp($password, $confirmPassword) !== 0) {
+        $_SESSION['error_msg'] = "Confirm password does not match the password.";
+        $valid = false;
+    }
+
+    return $valid;
+}
+
 $postData = file_get_contents("php://input");
-if (!empty($postData)) {
+if (!empty($postData) && isValid()) {
     $hostname = trim(filter_input(INPUT_POST, 'hostName', FILTER_SANITIZE_STRING));
     $firstName = trim(filter_input(INPUT_POST, 'firstName', FILTER_SANITIZE_STRING));
     $lastName = trim(filter_input(INPUT_POST, 'lastName', FILTER_SANITIZE_STRING));
