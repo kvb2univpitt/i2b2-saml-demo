3993da08a1aef51412e37a7b8b63262e9fde1afd
Kevin Bui <kvb2univpitt@gmail.com>
2021-10-19 15:25:34 -0400

    Updated encryption keys.

--- i2b2-idp-saml-demo/config/simplesamlphp/saml20-sp-remote.php
+++ i2b2-idp-saml-demo/config/simplesamlphp/saml20-sp-remote.php
@@ -49,53 +49,53 @@ $metadata['https://localhost/shibboleth'] = [
             'encryption' => false,
             'signing' => true,
             'type' => 'X509Certificate',
-            'X509Certificate' => 'MIID6zCCAlOgAwIBAgIJAKMwWSZwfEiDMA0GCSqGSIb3DQEBCwUAMBcxFTATBgNV
-                        BAMTDGNlMTQ1YTYyMTlhNDAeFw0yMTA4MjAxODEyMzBaFw0zMTA4MTgxODEyMzBa
-                        MBcxFTATBgNVBAMTDGNlMTQ1YTYyMTlhNDCCAaIwDQYJKoZIhvcNAQEBBQADggGP
-                        ADCCAYoCggGBAKloMuJpL8PI+kWtgktwFIwgwHpOqOuFSU+yCYqaFst+ESqQHwio
-                        1dO0AOIALBlvMKHfuSRoRCNjn/ouQwS/KcWMD1x+XnOU8Cvl15SkMhoQm3h4NSkt
-                        FsH8hfnJ7U8g+Iil9PhIewMjNiMMeDZKaNugyML5W0LiQ9XVowTe/O+7/DE/BkpU
-                        yZLQJ5WCc6R1OgmBzgiARVRlXM3CScCzlKu28sLAjKXbsvBoaiovanWI0NzoRm2i
-                        /UFyWH5zvKtENIN7c2afKLySR5bIQZ8ce39IOYtOsnNMMODPGLSl+78kEtZSXgmY
-                        9SdFxM3uFhLsO9eKq0lBKVDV8nmsjHCkQ9aDW8wZSupDoee1YNeTv+tmyxz0Y1Rj
-                        275UcEZpSFDj83uaeCWZq+wk15kNaW+srJuSn9YSef/i1awoqFtHg0Elq4mZ6rV6
-                        Cb3XtFlgMNjLpO+BF3gFjCXpnBp2hrZJ6Wnnb5tGKKJpwERhW5jBxbBJwr59rKd+
-                        Y+SM74XcKu2+qQIDAQABozowODAXBgNVHREEEDAOggxjZTE0NWE2MjE5YTQwHQYD
-                        VR0OBBYEFLPeqAINSTgADicI6QCwcFO9kmHrMA0GCSqGSIb3DQEBCwUAA4IBgQCD
-                        Nhf8fYqo7JB0gTzl0bspwe7762yMi6Hr27XttzVaifHemUGpV1AzEiQkqON5HkQ0
-                        /whk7gn10MUMTvgYQSGKJPJaUK5nTB5UY+KiymzpFCwdYtBAYrWyXP3WVfYUSlKq
-                        oPqE4A7+tLPBoAUm1uqR4tJQDg2atA9Ht697Ap5T9lig/aAe5Z8c8SPfT7npunO2
-                        vXIW7FH+cAVkSrpNBQjcXaENFgqccE27mrSrMF2MUqmqk9qA4LIZ7tGEPyc7hhLr
-                        2hMJjQmNJTuiBJg4gV9O05VQu/dVhZD9KH9qSN+tvviWozHAiruhhTMaG3Lm7+rz
-                        s+o+GmmPDHDOA7VM0F1eYDdnUu3HKaQAPp6yEQROfqjdlVluXA7jEzxRROovldsO
-                        rxuty+I+UPD4sf7YG081v6aZk0BTT1QIC8ERXVTpCtOQ5IiWCQg4YFetDmw1SK1p
-                        hSqz/sE9pYlc4lpbInXHbOTLNNSeWdQ2YZp8ep7bLp4aG7b15gAxtrYRmvFcops=',
+            'X509Certificate' => 'MIID6zCCAlOgAwIBAgIJANKLeZidZkNWMA0GCSqGSIb3DQEBCwUAMBcxFTATBgNV
+                        BAMTDDNmZDNjYWM2NDI4ZDAeFw0yMTEwMTkxNzEzNDZaFw0zMTEwMTcxNzEzNDZa
+                        MBcxFTATBgNVBAMTDDNmZDNjYWM2NDI4ZDCCAaIwDQYJKoZIhvcNAQEBBQADggGP
+                        ADCCAYoCggGBAKrHGUFdJXJ7YNre1/KwyHaEXz62DzaFWkqTZtLMcq+uotXf+aJt
+                        PvT5/kLOW60VkhXSqqaYoKmJjzRkff1vcgItL3ABlvSEbcQrJyHmzJl9DbMURjK9
+                        yfq6NcB2GqSK4UWfLoVcc/RamZBv69U0rsc/XBOmLCWV0zPqP90B1Dl+rj0Pf21u
+                        EJAAxM2LCBrZ7N2vk3Ykrlawt3o+6WF3lcSQFLxp2m+MpJRlGehzYeE/nskvsUqa
+                        DDdCLdaPDuOj/VVITIhuaL0Fb7mzKDBLpPL6ANqzuEUzJld3so6gBxJK+08rxEgu
+                        70StTRoIagZdHVupcEFjsGZUM5La9HztXYQyJOvSqE4wyL4aPWEo9tT7oiZApxdd
+                        XQKy6poNHKlEF6pGCyE+HSYDGQYax2gDMUFKzaOGhg+hc4QxKKSHflPymOURG4Tb
+                        40U0KqP0egki6leERvJDrR2SpnaGltjOheRMcbAEq37LGK8chaZMtjtO429kxvjR
+                        YrC+NFPioR4VYQIDAQABozowODAXBgNVHREEEDAOggwzZmQzY2FjNjQyOGQwHQYD
+                        VR0OBBYEFMVeDGqzIFHnCPUApDpLQf/8ckVAMA0GCSqGSIb3DQEBCwUAA4IBgQAl
+                        IYnC3Q6zfLMUcHiKG0PjWs6Z0aYqaSkXjCP4/twklUgG1nZJH7aai0AJF2jW4OpW
+                        0gCuHK2kcCbs6PaScy9QzNI53IcTC9rN+sy3ObibmZNUy68cyMBd+8DhDU1qNyAP
+                        FVgOugwJxyM7MNlm7fmkiCRGPzjCrjpabm2Yqj2Did0Nt1CmT7DOb1uxh3hESCTu
+                        xv3gwQLDUB/NTKQuskurpv3LsYj1gzQDnS68QAixYz9yB3IVwBpesiBMKCQrAoUB
+                        SBAJ23xSluBXK9U7pJa955aKJkMiQ4XSZAEu+ODBLg6mv5PtPxfDi0N9HCN7iZUx
+                        8xa2cr9IKPHa6rIS62o3Zw25OYzWcNqK6+gl5ueO50FU2OduwLZY/Nm94NVN0SXI
+                        ND+iue1srM8aK5sfTq5B9rIJ0ShcEtXyVql1so8cMQ5jebzNhWkkvgVAhLOf/9ts
+                        mqGs4B9ayjdo/sbg2w4FZfub4rzCORG67KYicIgAYz+DViJN2IoV96FAMWzh4Ig=',
         ],
         [
             'encryption' => true,
             'signing' => false,
             'type' => 'X509Certificate',
-            'X509Certificate' => 'MIID6zCCAlOgAwIBAgIJAN0uYflgzBhSMA0GCSqGSIb3DQEBCwUAMBcxFTATBgNV
-                        BAMTDGNlMTQ1YTYyMTlhNDAeFw0yMTA4MjAxODEyMzBaFw0zMTA4MTgxODEyMzBa
-                        MBcxFTATBgNVBAMTDGNlMTQ1YTYyMTlhNDCCAaIwDQYJKoZIhvcNAQEBBQADggGP
-                        ADCCAYoCggGBAJu47y5bJcUpRABzvSUtxgpxw17UbxxlL9jZ7ZN7rKFDz19BZsHx
-                        mkcgof5QqwSVMmZKwz+jLHWtnldEfEDzqnA7YvB1JrIAqHA197zqM8ocFVptjUB1
-                        z3bvU/em7x3aHM/5aDS51Txoh6zTpEfPSxRSGKiw0rgxwMRdXUE4jzXmvHhHXGbz
-                        iDhKTcB7e7tPPoxJhpWJkoRiM+FsionWSrIEkyZjvRFYGHzdHamgVRx2cpI3/F1H
-                        4PN1dGsVOGY6j21HGfrQuVqfEEFh0QQMxjI0jN7m6ZhQM8STgpzlLbkue0FFc+1d
-                        pLV0DxdcEkv8X0GTWDvzMhwjcSIveCYk/jr+NNge2s85cGx8kzUTCVDrJIUpjf5q
-                        RyMxKOrW1PEkV9J6uuRdmo4W9VAS8ef6Z6FIIr1G70IlYbH64bKJ4+IirM3mymcw
-                        2QBgzB8ak3j3tx0XpWPd2i7BQ9v+fOOAU8vClmJIpW8inpFUn7D/m25V/WVKvfXo
-                        Ts3qDTJRRMl+5QIDAQABozowODAXBgNVHREEEDAOggxjZTE0NWE2MjE5YTQwHQYD
-                        VR0OBBYEFO1KmERbH4uQ7fqQ+ZbZltxgl9+zMA0GCSqGSIb3DQEBCwUAA4IBgQCa
-                        e5DHsNSEFESn7A+1N6x37jHcHBXLLLxqvHoxlho1jaZcvsTuI8FjBPKbKmnU9Eo0
-                        +f1ESMGq7U7kLCuWPOgVqOq3ozu8KnRIWoxC95V3/N/qFz09Cx7MPpothr9Q8wjd
-                        UOqm9gkT6D/4DSfGZt6IihPVEfcBFvsbxSVHH4oYhUvBCZfw9AZHuyZL3GleolXF
-                        OqBdsqjAbbJ2/VxcUqjWOvNEVSr0mGXO4lqQESSvKSdMhwiD+KPVmTrZtm7CVOYu
-                        nHo2yUX41lqkPGXib+7R/WJhZcAYMPmrDaYccsaOV+vhCEzC3mRieuzCzKqZ0MlP
-                        FJU8vMkxWLh9TwQF/eIcPh5lTJSIVv5Yp8XWS+/N1tow9OKrc7V8teEK6xc4btmD
-                        8bZB/WZQ+W6PYsi7g/meBDMS/VBjzX0yngh+HCvM3P3cpYL7UjolpF+HEdkCfJx1
-                        ksuD2P9/4ooswBMjSs4AP1edjswGnqf7ZMTPnwbPERJ8J+ujsfJ/GAB5zFOvRp8=',
+            'X509Certificate' => 'MIID6zCCAlOgAwIBAgIJAOKS8g1D1F8JMA0GCSqGSIb3DQEBCwUAMBcxFTATBgNV
+                        BAMTDDNmZDNjYWM2NDI4ZDAeFw0yMTEwMTkxNzEzNDZaFw0zMTEwMTcxNzEzNDZa
+                        MBcxFTATBgNVBAMTDDNmZDNjYWM2NDI4ZDCCAaIwDQYJKoZIhvcNAQEBBQADggGP
+                        ADCCAYoCggGBAL0G/XNetiFaG0vYw6VI2c6IBCFD4G9BMr7zKTMacC4SOKwGzEyx
+                        o9QJSisB77ZnYltMO86lABuI3+gW+pnVeyNe4rviHWoa8ifCvOv1ah/XkIutH26m
+                        XHwc7n6SWjIM7E8W09ZbQVZPEAdGxa80SLGgSE3ZR4tzkvyRIBJStv+XYMVY5kf2
+                        ijmn5/6bYeEPmXSUVgz6qvejQ0D7AUz08Zf1gOeGtcvxeFAtfJEMEl1O0qdCBC33
+                        Fu0JS8pHsQ5moX48ctF8sXox3OPY+gGgW1ea4TPUXCJMQ0zWHJtkaoNP/k9Z4iFc
+                        1wpXtXSUa3F8toCbEpcm3HNtYujfV+nLC4dIBJUuZe3/Af3CxuWw/KQM3rVYoUop
+                        spYuqhIA39y7KBTgP6QJiWoGkVEsYdBrmixsg/jwG3ZSPqdDYrsKSDWNGBBdEyuj
+                        2TtBLlId55ngbeILY2Y6anAobfEbGFHd4XtORSywjpnNwvpfPuCN8AF0dejxA7Ky
+                        9usdshsUWjdETQIDAQABozowODAXBgNVHREEEDAOggwzZmQzY2FjNjQyOGQwHQYD
+                        VR0OBBYEFIKWRu1MS1WTmlSaoyBl8AEcbsLhMA0GCSqGSIb3DQEBCwUAA4IBgQAq
+                        xhcar4YUNtqjKMH7ZzT8MHlhq8M8A/odo0nplTYQsQS2IQZcSDDnAeydectDIQge
+                        PACh70SPVTZwv+2CNtQT6gvq4heRTAj82Tf0W70fczZNtQCL9u4ydgx+TjNEvcKr
+                        AKVJ1sQdrXrFgUlojFQUBITCLelBeHS7pVUfkiaNgJBnqwrpqKqMrsfpoRiQxOe1
+                        oVhAmXoPTqW5PFtovZyf204rqaskWKJgfqGXzg806kjIKJl3cJkwmB2td7CzHiRB
+                        yLHGM1k8U8pW6Ay5eg2XcFdhlpQLUMbECOv5UbaZjewHDnxMtlEYAXOq0l6YSj5i
+                        umBB5FsD7ctzQji8HToSlrOAJMLWvfG+EQPUEqk8AfifR0C/HCwc2KO04a0Dofm7
+                        oGoKhO+AKGVmzACZnpC/BqyFvVQPo1K/7Ok6PMk/cWCADnTe5YKbi2So3bcN9yVO
+                        V1Fv3EhP4lkaC770W+EMlcYr44xOjiIll0BTnk0/Wzaw9JDD44mmCGyJzwugFWU=',
         ],
     ],
     'validate.authnrequest' => true,
